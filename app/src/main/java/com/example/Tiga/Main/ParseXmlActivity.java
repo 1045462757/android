4c34da7a45c2955afc0bc9d1a1c5d5757764936a
tiga <1045462757@qq.com>
2018-10-29 16:35:14 +0800

    解析xml数据

---
+++ app/src/main/java/com/example/Tiga/Main/ParseXmlActivity.java
@@ -0,0 +1,13 @@
+package com.example.Tiga.Main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParseXmlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parse_xml);
+    }
+}
